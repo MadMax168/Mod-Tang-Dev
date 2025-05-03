34f6ac7875cf69194306bfb8f358b9fb5861e14c
unknown <phonvarit@gmail.com>
2025-05-03 14:15:01 +0700

    fix code algorithm.java

---
+++ app/src/main/java/mod/tang/dev/ControlPoint.java
@@ -0,0 +1,10 @@
+package mod.tang.dev;
+
+public class ControlPoint {
+    public double x, y;
+
+    public ControlPoint(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
---
+++ app/src/main/java/mod/tang/dev/Node.java
@@ -0,0 +1,14 @@
+package mod.tang.dev;
+
+public class Node {
+    public double x, y;
+    public String label;
+    public boolean isSpecial;
+
+    public Node(double x, double y, String label, Boolean isSpecial) {
+        this.x = x;
+        this.y = y;
+        this.label = label;
+        this.isSpecial = isSpecial;
+    }
+}
